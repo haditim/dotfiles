e24d521382ce9c61999a8738e52c272badb4e5f9
Hadi Timachi <hadi@timachi.com>
2023-04-25 21:46:46 +0200

    create fish config

---
+++ .config/fish/conf.d/alias.fish
@@ -0,0 +1,78 @@
+# git
+abbr -a gs  git status -sb
+abbr -a ga  git add
+abbr -a gc  git commit
+abbr -a gcm git commit -m
+abbr -a gca git commit --amend
+abbr -a gcl git clone
+abbr -a gco git checkout
+abbr -a gp  git push
+abbr -a gpl git pull
+abbr -a gl  git l
+abbr -a gd  git diff
+abbr -a gds git diff --staged
+abbr -a gr  git rebase -i HEAD~15
+abbr -a gf  git fetch
+abbr -a gfc git findcommit
+abbr -a gfm git findmessage
+abbr -a gco git checkout
+
+# ls
+alias l='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+alias lt='ls --tree'
+
+# misc
+alias reload='exec fish'
+
+# dotfiles
+alias dotfiles='/usr/bin/git --git-dir=/home/hadi/.dotfiles/ --work-tree=/home/hadi'
+
+# docker
+# Get latest container ID
+alias dl="docker ps -l -q"
+# Get container process
+alias dps="docker ps"
+# Get process included stop container
+alias dpa="docker ps -a"
+# Get images
+alias di="docker images"
+# Get container IP
+alias dip="docker inspect --format '{{ .NetworkSettings.IPAddress }}'"
+# Run deamonized container, e.g., $dkd base /bin/echo hello
+alias dkd="docker run -d -P"
+# Run interactive container, e.g., $dki base /bin/bash
+alias dki="docker run -i -t -P"
+# Execute interactive container, e.g., $dex base /bin/bash
+alias dex="docker exec -i -t"
+# Stop all containers
+function dstop
+    docker stop $(docker ps -a -q)
+end
+# Remove all containers
+function drm
+    docker rm $(docker ps -a -q)
+end
+# Stop and Remove all containers
+alias drmf='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
+# Remove all images
+function dri
+    docker rmi $(docker images -q)
+end
+# Dockerfile build, e.g., $dbu tcnksm/test
+function dbu
+    docker build -t=$1 .
+end
+# Bash into running container
+function dbash
+    docker exec -it $(docker ps -aqf "name=$1") bash
+end
+alias dco="docker compose"
+
+# python
+function pyclean
+    find . -type f -name '*.py[co]' \
+    -delete -o -type d -name __pycache__ \
+    -delete -o -type d -name .mypy_cache -exec rm -r {} +
+end
---
+++ .config/fish/conf.d/main.fish
@@ -0,0 +1,76 @@
+# Fish syntax highlighting
+set -g fish_color_autosuggestion '555'  'brblack'
+set -g fish_color_cancel -r
+set -g fish_color_command --bold
+set -g fish_color_comment red
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+set -g fish_color_end brmagenta
+set -g fish_color_error brred
+set -g fish_color_escape 'bryellow'  '--bold'
+set -g fish_color_history_current --bold
+set -g fish_color_host normal
+set -g fish_color_match --background=brblue
+set -g fish_color_normal normal
+set -g fish_color_operator bryellow
+set -g fish_color_param cyan
+set -g fish_color_quote yellow
+set -g fish_color_redirection brblue
+set -g fish_color_search_match 'bryellow'  '--background=brblack'
+set -g fish_color_selection 'white'  '--bold'  '--background=brblack'
+set -g fish_color_user brgreen
+set -g fish_color_valid_path --underline
+
+# Set different defaults if in emacs
+if [ "$INSIDE_EMACS" = 'vterm' ]
+    export EDITOR=emacsclient
+    export VISUAL=emacsclient
+    export PAGER=cat
+
+    alias magit="emacsclient -ne '(magit-status)'"
+
+    function man
+        emacsclient -ne "(man \"$1\")"
+    end
+
+    # VTERM
+    function vterm_printf;
+        if begin; [  -n "$TMUX" ]  ; and  string match -q -r "screen|tmux" "$TERM"; end
+            # tell tmux to pass the escape sequences through
+            printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
+        else if string match -q -- "screen*" "$TERM"
+            # GNU screen (screen, screen-256color, screen-256color-bce)
+            printf "\eP\e]%s\007\e\\" "$argv"
+        else
+            printf "\e]%s\e\\" "$argv"
+        end
+    end
+    function clear
+        vterm_printf "51;Evterm-clear-scrollback";
+        tput clear;
+    end
+    function fish_title
+        hostname
+        echo ":"
+        prompt_pwd
+    end
+    function vterm_prompt_end;
+        vterm_printf '51;A'(whoami)'@'(hostname)':'(pwd)
+    end
+    functions --copy fish_prompt vterm_old_fish_prompt
+    function fish_prompt --description 'Write out the prompt; do not replace this. Instead, put this at end of your file.'
+        printf "%b" (string join "\n" (vterm_old_fish_prompt))
+        vterm_prompt_end
+    end
+    function vterm_cmd --description 'Run an Emacs command among the ones been defined in vterm-eval-cmds.'
+        set -l vterm_elisp ()
+        for arg in $argv
+            set -a vterm_elisp (printf '"%s" ' (string replace -a -r '([\\\\"])' '\\\\\\\\$1' $arg))
+        end
+        vterm_printf '51;E'(string join '' $vterm_elisp)
+    end
+end
+
+
+# Run Starship
+starship init fish | source
