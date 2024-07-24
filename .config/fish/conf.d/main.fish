023cd175c0d556d65e193c640231d7263b538dc0
Hadi Timachi <hadi@timachi.com>
2024-07-24 19:16:06 +0200

    remove extra code from fish

--- .config/fish/conf.d/main.fish
+++ .config/fish/conf.d/main.fish
@@ -82,29 +82,5 @@ function mark_prompt_start --on-event fish_prompt
 end
 # direnv hook fish | source
 
-# C-x C-e to edit command in editor (similar to bash)
-function edit_command_buffer --description 'Edit the command buffer in an external editor'
-    set -l f (mktemp)
-    if set -q f[1]
-        mv $f $f.fish
-        set f $f.fish
-    else
-        # We should never execute this block but better to be paranoid.
-        set f /tmp/fish.(echo %self).fish
-        touch $f
-    end
-
-    set -l p (commandline -C)
-    commandline -b > $f
-    if set -q EDITOR
-        eval $EDITOR $f
-    else
-        vim $f
-    end
-
-    commandline -r (cat $f)
-    commandline -C $p
-    command rm $f
-end
-
+# C-x C-e to edit command in editor (similar to bash) (also M-e and M-v in fish)
 bind \cx\ce edit_command_buffer
